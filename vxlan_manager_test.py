19d56844eaecd2c58e04aa28f87e73db15205b62
Ofer Chen <oferchen@gmail.com>
2023-11-02 22:56:08 +0100

    Update vxlan_manager_test.py

--- vxlan_manager_test.py
+++ vxlan_manager_test.py
@@ -1,40 +1,81 @@
 import unittest
-from unittest.mock import patch, Mock
-from vxlan_manager import VXLANManager, create_vxlan_tunnel, cleanup_vxlan_tunnel
+from unittest.mock import patch, MagicMock
+from vxlan_manager import VXLANManager
 
 class TestVXLANManager(unittest.TestCase):
+
     def setUp(self):
-        pass
+        self.vxlan_manager_ip = VXLANManager(bridge_tool='ip')
+        self.vxlan_manager_brctl = VXLANManager(bridge_tool='brctl')
 
     def tearDown(self):
         pass
 
-    @patch('subprocess.call')
-    @patch('subprocess.Popen')
-    def test_create_vxlan_interface(self, mock_call, mock_popen):
-        mock_call.side_effect = [0, 0, 0]  # Simulate successful subprocess calls
-        mock_popen.return_value = Mock()  # Mock the subprocess.Popen object
+    @patch('subprocess.run')
+    def test_create_vxlan_interface_ip_success(self, mock_run):
+        mock_run.return_value = MagicMock(returncode=0)
 
         vni = 1001
         src_host = '10.0.0.1'
         dst_host = '10.0.0.2'
         bridge_name = 'br0'
-        src_port = 4789
-        dst_port = 4789
 
-        vxlan_manager = VXLANManager()
-        vxlan_manager.create_vxlan_interface(vni, src_host, dst_host, bridge_name, src_port, dst_port)
+        self.vxlan_manager_ip.create_vxlan_interface(vni, src_host, dst_host, bridge_name)
 
-        mock_call.assert_called_with(['ip', 'link', 'add', f'vxlan{vni}', 'type', 'vxlan',
-                                     'id', str(vni),
-                                     'local', src_host,
-                                     'remote', dst_host,
-                                     'dev', 'eth0',
-                                     'dstport', str(src_port)])
-        mock_call.assert_called_with(['ip', 'link', 'set', f'vxlan{vni}', 'up'])
-        mock_call.assert_called_with(['brctl', 'addif', bridge_name, f'vxlan{vni}'])
+        calls = [
+            patch.call(['ip', 'link', 'add', f'vxlan{vni}', 'type', 'vxlan',
+                        'id', str(vni),
+                        'local', src_host,
+                        'remote', dst_host,
+                        'dev', 'eth0',
+                        'dstport', str(self.vxlan_manager_ip.default_src_port)], check=True),
+            patch.call(['ip', 'link', 'set', f'vxlan{vni}', 'up'], check=True),
+            patch.call(['ip', 'link', 'set', 'master', bridge_name, f'vxlan{vni}'], check=True)
+        ]
+        mock_run.assert_has_calls(calls, any_order=True)
 
-    # Write similar test cases for other methods
-    
+    @patch('subprocess.run')
+    def test_create_vxlan_interface_ip_failure(self, mock_run):
+        mock_run.side_effect = subprocess.CalledProcessError(1, 'ip')  # Simulate a failure
+
+        vni = 1001
+        src_host = '10.0.0.1'
+        dst_host = '10.0.0.2'
+        bridge_name = 'br0'
+
+        with self.assertRaises(SystemExit) as cm:
+            self.vxlan_manager_ip.create_vxlan_interface(vni, src_host, dst_host, bridge_name)
+
+        self.assertEqual(cm.exception.code, 1)
+
+    @patch('subprocess.run')
+    def test_cleanup_vxlan_interface_brctl_success(self, mock_run):
+        mock_run.return_value = MagicMock(returncode=0)
+
+        vni = 1001
+        bridge_name = 'br0'
+
+        self.vxlan_manager_brctl.cleanup_vxlan_interface(vni, bridge_name)
+
+        calls = [
+            patch.call(['brctl', 'delif', bridge_name, f'vxlan{vni}'], check=True),
+            patch.call(['ip', 'link', 'del', f'vxlan{vni}'], check=True)
+        ]
+        mock_run.assert_has_calls(calls, any_order=True)
+
+    @patch('subprocess.run')
+    def test_cleanup_vxlan_interface_brctl_failure(self, mock_run):
+        mock_run.side_effect = subprocess.CalledProcessError(1, 'brctl')  # Simulate a failure
+
+        vni = 1001
+        bridge_name = 'br0'
+
+        with self.assertRaises(SystemExit) as cm:
+            self.vxlan_manager_brctl.cleanup_vxlan_interface(vni, bridge_name)
+
+        self.assertEqual(cm.exception.code, 1)
+
+    # Write additional test cases for other methods, scenarios, and bridge tools
+
 if __name__ == '__main__':
     unittest.main()
