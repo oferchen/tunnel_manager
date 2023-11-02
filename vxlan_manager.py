aea4749868d5749c21b91a257e670b96693f807e
Ofer Chen <oferchen@gmail.com>
2023-11-02 23:24:53 +0100

    added list method and output formats

--- vxlan_manager.py
+++ vxlan_manager.py
@@ -1,8 +1,12 @@
 import argparse
+import csv
+import json
 import logging
+import re
 import socket
 import subprocess
 import sys
+from xml.etree import ElementTree
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -55,31 +59,87 @@ class VXLANManager:
             except socket.error as e:
                 logger.error(f'Failed to establish connectivity to VXLAN VNI {vni} at {dst_host}:{port} from {src_host}: {e}')
 
+    def list_vxlan_interfaces(self, fields, output_format):
+        # Execute the command to list VXLAN interfaces
+        result = subprocess.run(['ip', '-d', 'link', 'show', 'type', 'vxlan'], stdout=subprocess.PIPE, text=True)
+        vxlan_interfaces = []
+
+        # Regular expression to match VXLAN interface details
+        vxlan_regex = re.compile(r'(?P<ifname>\S+): .+ vxlan id (?P<vni>\d+) .+ '
+                                 r'local (?P<src_host>\S+) remote (?P<dst_host>\S+|\d+\.\d+\.\d+\.\d+) '
+                                 r'.+ dstport (?P<dst_port>\d+)')
+
+        # Split the output into lines and match each line with the regex
+        for line in result.stdout.split('\n'):
+            match = vxlan_regex.search(line)
+            if match:
+                details = match.groupdict()
+                # If fields is 'all' or the field is in the specified fields, include it in the output
+                vxlan_details = {key: value for key, value in details.items() if fields == 'all' or key in fields}
+                vxlan_interfaces.append(vxlan_details)
+
+        # Format the output based on the requested format
+        return OutputFormatter.format(vxlan_interfaces, output_format)
+
+class OutputFormatter:
+    @staticmethod
+    def format(data, format_type):
+        if format_type == 'json':
+            return json.dumps(data, indent=2)
+        elif format_type == 'xml':
+            root = ElementTree.Element('VXLANInterfaces')
+            for item in data:
+                interface_element = ElementTree.SubElement(root, 'Interface')
+                for key, value in item.items():
+                    element = ElementTree.SubElement(interface_element, key)
+                    element.text = str(value)
+            return ElementTree.tostring(root, encoding='unicode')
+        elif format_type == 'csv':
+            if not data:
+                return ""
+            csv_output = io.StringIO()
+            writer = csv.DictWriter(csv_output, fieldnames=data[0].keys())
+            writer.writeheader()
+            writer.writerows(data)
+            return csv_output.getvalue()
+        elif format_type == 'string':
+            return ', '.join([': '.join([key, str(val)]) for item in data for key, val in item.items()])
+        elif format_type == 'table':
+            table = str()
+            headers = data[0].keys() if data else []
+            table += ' | '.join(headers) + '\n'
+            table += '-+-'.join(['-' * len(header) for header in headers]) + '\n'
+            for item in data:
+                table += ' | '.join(item.values()) + '\n'
+            return table
+
 def main():
-    parser = argparse.ArgumentParser(description='Create or remove a VXLAN tunnel between two bridges.')
-    parser.add_argument('--vni', type=int, required=True, help='VXLAN VNI')
-    parser.add_argument('--src-host', required=True, help='Source host IP')
-    parser.add_argument('--dst-host', required=True, help='Destination host IP')
-    parser.add_argument('--bridge-name', required=True, help='Bridge name to add VXLAN interface')
+    parser = argparse.ArgumentParser(description='Manage VXLAN tunnels between bridges.')
+    parser.add_argument('--vni', type=int, help='VXLAN VNI')
+    parser.add_argument('--src-host', help='Source host IP')
+    parser.add_argument('--dst-host', help='Destination host IP')
+    parser.add_argument('--bridge-name', help='Bridge name to add VXLAN interface')
     parser.add_argument('--src-port', type=int, help='Source VXLAN UDP port')
     parser.add_argument('--dst-port', type=int, help='Destination VXLAN UDP port')
     parser.add_argument('--dev', default='eth0', help='Network device (default: eth0)')
     parser.add_argument('--cleanup', action='store_true', help='Remove VXLAN tunnel instead of creating it')
     parser.add_argument('--validate-connectivity', action='store_true', help='Perform post-deployment connectivity validation')
+    parser.add_argument('--list', action='store_true', help='List VXLAN interfaces')
+    parser.add_argument('--fields', nargs='+', default='all', help='Fields to include when listing VXLAN interfaces')
+    parser.add_argument('--format', choices=['json', 'xml', 'csv', 'string', 'table'], default='table', help='Output format when listing VXLAN interfaces')
     parser.add_argument('--bridge-tool', choices=['ip', 'brctl'], default='ip', help='Bridge management tool to use (default: ip)')
     args = parser.parse_args()
 
     vxlan_manager = VXLANManager(bridge_tool=args.bridge_tool)
 
-    if args.cleanup:
+    if args.list:
+        print(vxlan_manager.list_vxlan_interfaces(fields=args.fields, output_format=args.format))
+    elif args.cleanup:
         vxlan_manager.cleanup_vxlan_interface(args.vni, args.bridge_name)
-        logger.info(f'VXLAN tunnel {args.vni} removed successfully.')
+    elif args.validate_connectivity:
+        vxlan_manager.validate_connectivity(args.src_host, args.dst_host, args.vni, args.src_port)
     else:
         vxlan_manager.create_vxlan_interface(args.vni, args.src_host, args.dst_host, args.bridge_name, args.src_port, args.dst_port, args.dev)
-        logger.info(f'VXLAN tunnel {args.vni} created successfully.')
-
-        if args.validate_connectivity:
-            vxlan_manager.validate_connectivity(args.src_host, args.dst_host, args.vni, args.src_port)
 
 if __name__ == '__main__':
     main()
